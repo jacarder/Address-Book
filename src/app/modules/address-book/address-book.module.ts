b244accc7f647ba81530b19115be1fd8a1582ab4
Jacarder <jonandrewcarder@gmail.com>
2021-10-06 23:37:56 -0400

    Address card scaffold

--- src/app/modules/address-book/address-book.module.ts
+++ src/app/modules/address-book/address-book.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddressBookComponent } from './components/address-book/address-book.component';
 import { AddressBookRoutingModule } from './address-book-routing.module';
+import { AddressCardComponent } from './components/address-card/address-card.component';
 
 
 
 @NgModule({
   declarations: [
-    AddressBookComponent
+    AddressBookComponent,
+    AddressCardComponent
   ],
   imports: [
     CommonModule,
