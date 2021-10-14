64d0263284356f18dca62bdd764002d0852db995
Jacarder <jonandrewcarder@gmail.com>
2021-10-14 12:05:14 -0400

    Address-Book tests created

--- src/app/modules/address-book/components/address-book/address-book.component.spec.ts
+++ src/app/modules/address-book/components/address-book/address-book.component.spec.ts
@@ -4,20 +4,32 @@ import { FilterQuery } from '../../state/filter.query';
 import {MatCardModule} from '@angular/material/card';
 
 import { AddressBookComponent } from './address-book.component';
+import { of } from 'rxjs';
+import { AddressEntry, createAddressBookEntry } from '../../state/address-Entry.model';
+import { AddressBookState, AddressBookStore } from '../../state/address-book.store';
+import { AddressBookService } from '../../state/address-book.service';
+import { FilterService } from '../../state/filter.service';
+import { FilterStore } from '../../state/filter.store';
+import { AddressCardComponent } from '../address-card/address-card.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AddressBookComponent', () => {
   let component: AddressBookComponent;
   let fixture: ComponentFixture<AddressBookComponent>;
+  let addressBookStore: AddressBookStore;
+  let filterStore: FilterStore;
+  let addressEntry: AddressEntry;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        MatCardModule
+        MatCardModule,
+        RouterTestingModule
       ],
-      declarations: [ AddressBookComponent ],
+      declarations: [ AddressBookComponent, AddressCardComponent ],
       providers: [
-        AddressBookQuery,
-        FilterQuery
+        AddressBookService,
+        FilterService
       ]
     })
     .compileComponents();
@@ -26,18 +38,54 @@ describe('AddressBookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AddressBookComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    addressBookStore = TestBed.inject(AddressBookStore);
+    filterStore = TestBed.inject(FilterStore);
+    addressEntry = {
+      id: '',
+      name: {
+        first: '',
+        last: ''
+      },
+      location: {
+        street: '',
+        city: '',
+        state: '',
+        postcode: '',
+      },
+      email: '',
+      phone: '',
+      cell: '',
+      picture: {
+        large: '',
+        medium: '',
+        thumbnail: ''
+      } 
+    }  
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  //  Begin test cards
+  it('given no addresses, should render error message no addresses found', () => {
+    addressBookStore.update({addresses: []})
+    fixture.detectChanges();
+    const matCard: Element = fixture.debugElement.nativeElement.querySelector('mat-card');
+    const actual = 'No addresses found with search result';
+    const expected = matCard.innerHTML;
+    expect(expected).toBe(actual);
+  });
 
-
-  // it('should render error message no addresses found', () => {
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('.content span')?.textContent).toContain('address-book app is running!');
-  // });
+  it('given two address, should render two address-cards', () => {
+    let addressBookState: AddressBookState = {
+      addresses: [addressEntry, addressEntry]
+    }
+    addressBookStore.update(addressBookState);      
+    fixture.detectChanges();
+    const cards: Element[] = fixture.debugElement.nativeElement.querySelectorAll('address-card');
+    const actual = 2;
+    const expected = cards.length;
+    expect(expected).toEqual(actual);
+  });  
 
 });
