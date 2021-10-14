fcf30d3570d3897b326ed291672d94b5b34763fe
Jacarder <jonandrewcarder@gmail.com>
2021-10-14 10:33:42 -0400

    mat-card-title tests created and html modified based on findings

--- src/app/modules/address-book/components/address-card/address-card.component.spec.ts
+++ src/app/modules/address-book/components/address-card/address-card.component.spec.ts
@@ -2,12 +2,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import { AddressCardComponent } from './address-card.component';
-import { createAddressBookEntry } from '../../state/address-Entry.model';
+import { AddressEntry, createAddressBookEntry } from '../../state/address-Entry.model';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AddressCardComponent', () => {
   let component: AddressCardComponent;
   let fixture: ComponentFixture<AddressCardComponent>;
+  let addressEntry: AddressEntry;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -24,7 +25,7 @@ describe('AddressCardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AddressCardComponent);
     component = fixture.componentInstance;
-    let addressEntry = {
+    addressEntry = {
       id: '',
       name: {
         first: '',
@@ -52,4 +53,33 @@ describe('AddressCardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  //  Begin testing for mat-card-title
+  it('given a name object should render mat-card-title with name', () => {
+    component.addressEntry.name.first = "John";
+    component.addressEntry.name.last = "Doe";
+    fixture.detectChanges();
+    const matTitle: Element = fixture.debugElement.nativeElement.querySelector('mat-card-title');
+    const actual = 'John Doe';
+    const expected = matTitle.innerHTML;
+    expect(expected).toBe(actual);
+  });
+  
+  it('given a name object with blank string for first and last namem should render mat-card-title with title "No name on file"', () => {
+    component.addressEntry.name.first = '';
+    component.addressEntry.name.last = '';
+    fixture.detectChanges();
+    const matTitle: Element = fixture.debugElement.nativeElement.querySelector('mat-card-title');
+    const actual = 'No name on file';
+    const expected = matTitle.innerHTML;
+    expect(expected).toBe(actual);
+  });
+
+  it('given component renders, should only show one mat-card-title', () => {
+    const matTitle: Element[] = fixture.debugElement.nativeElement.querySelectorAll('mat-card-title');
+    const actual = 1;
+    const expected = matTitle.length;
+    expect(expected).toBe(actual);
+  });  
+
 });
