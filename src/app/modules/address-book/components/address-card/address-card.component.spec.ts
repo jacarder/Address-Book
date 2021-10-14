33b3a2fab06c9a9db0716b738345c0d5462b055c
Jacarder <jonandrewcarder@gmail.com>
2021-10-14 11:00:38 -0400

    location tests created & updated html based on tests

--- src/app/modules/address-book/components/address-card/address-card.component.spec.ts
+++ src/app/modules/address-book/components/address-card/address-card.component.spec.ts
@@ -81,5 +81,29 @@ describe('AddressCardComponent', () => {
     const expected = matTitle.length;
     expect(expected).toBe(actual);
   });  
+  //  Begin testing for location
+  it('given a location object should render paragraph element with address', () => {
+    component.addressEntry.location.street = "text1";
+    component.addressEntry.location.city = "text2";
+    component.addressEntry.location.state = "text3";
+    component.addressEntry.location.postcode = "text4";
+    fixture.detectChanges();
+    const matContent: Element[] = fixture.debugElement.nativeElement.querySelectorAll('p');
+    const actual = 'text1 text2, text3 text4';
+    const expected = `${matContent[0].innerHTML} ${matContent[1].innerHTML}`;
+    expect(expected).toBe(actual);
+  });
+  
+  it('given a location object with blank string for all properties, should render paragraph element with title "No location on file"', () => {
+    component.addressEntry.location.street = "";
+    component.addressEntry.location.city = "";
+    component.addressEntry.location.state = "";
+    component.addressEntry.location.postcode = "";
+    fixture.detectChanges();
+    const matTitle: Element = fixture.debugElement.nativeElement.querySelector('p');
+    const actual = 'No location on file';
+    const expected = matTitle.innerHTML;
+    expect(expected).toBe(actual);
+  });
 
 });
